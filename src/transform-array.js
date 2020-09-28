7d5410da983aef93fb30361a3f71b9d19bb2278f
tatsmaki <61022192+tatsmaki@users.noreply.github.com>
2020-09-28 21:26:10 +0300

    fix: solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,23 +1,31 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr) {
-  console.log(arr);
-  let orr=arr;
-  for (let i=0; i<orr.length; i++){
-    switch(orr[i]) { 
-      case '--double-next': if (orr[i+1]!=undefined) orr.splice(i,1,orr[i+1]); else orr.splice(i,1); break;
-      case '--double-prev': if (orr[i-1]!=undefined && orr[i-2]!='--discard-next') orr.splice(i,1,orr[i-1]); else orr.splice(i,1); break;
-      default: break;
+  if (!Array.isArray(arr)) {
+    throw new Error();
+  }
+  let a = [];
+  for (let i=0; i<arr.length; i++){
+    switch(arr[i]) { 
+      case '--double-next': 
+        if (arr[i+1]!=undefined) 
+          a.push(arr[i+1]); 
+      break;
+      case '--double-prev': 
+        if (arr[i-1]!=undefined && arr[i-2]!='--discard-next') 
+          a.push(arr[i-1]); 
+      break;
+      case '--discard-next':
+        i++;
+      break;
+      case '--discard-prev':
+        if (arr[i-2]!='--discard-next')
+          a.pop();
+      break;
+      default: 
+        a.push(arr[i]);
+      break;
     }
   } 
- 
-  for (let i=0; i<orr.length; i++){
-    switch(orr[i]) { 
-      case '--discard-next': if (orr[i+1]!=undefined && orr[i+2]!='--discard-prev') orr.splice(i,2); else orr.splice(i,1); break;
-      case '--discard-prev': if (orr[i-1]!=undefined && orr[i-2]!='--discard-next') orr.splice(i-1,2); else orr.splice(i,1); break;
-      default: break;
-    }
-  }
-  console.log(orr);
-  return orr;
+  return a;
 };
