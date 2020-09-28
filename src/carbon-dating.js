91b0e253970d87cb2045da6f9cb42f7986b098bc
tatsmaki <61022192+tatsmaki@users.noreply.github.com>
2020-09-28 18:38:13 +0300

    fix: float activity

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,8 +4,10 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
-	sampleActivity=Number(sampleActivity);
-	if(typeof sampleActivity!=Number)
-		return false;
-	return Math.ceil(HALF_LIFE_PERIOD/0.693*Math.log(MODERN_ACTIVITY/sampleActivity));
+	if(typeof sampleActivity == "string"){
+		sampleActivity = parseFloat(sampleActivity);
+		if(MODERN_ACTIVITY > sampleActivity && sampleActivity > 0)
+      			return Math.ceil(HALF_LIFE_PERIOD/0.693 * Math.log(MODERN_ACTIVITY / sampleActivity));
+  	}
+  	return false;
 };
