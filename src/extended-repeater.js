88a08c0aaa4fcecca52741d48217627c73c8aef8
tatsmaki <61022192+tatsmaki@users.noreply.github.com>
2020-09-28 22:26:48 +0300

    feat: nice nice very nice

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,31 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let answer=[], repeatTime=0;
+  if (str===null)
+    str='null';
+  if (options.addition===null)
+    options.addition='null';
+  if (options.repeatTimes===undefined){
+    answer.push(str);
+    answer.push(options.addition);
+  }
+  if (options.separator===undefined)
+    options.separator='+';
+  if (options.additionSeparator===undefined)
+    options.additionSeparator='|';
+  while (repeatTime < options.repeatTimes) {
+    if (repeatTime!==0)
+      answer.push(options.separator);
+    answer.push(str); 
+    for (let additionRepeatTime=0; additionRepeatTime<options.additionRepeatTimes; additionRepeatTime++){
+      if (additionRepeatTime!==0)
+        answer.push(options.additionSeparator);
+      answer.push(options.addition);
+    }
+    repeatTime++;
+  }
+  answer=answer.join('');
+  return answer;
 };
   
